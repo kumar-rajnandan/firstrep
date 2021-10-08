ec6956a2cb9252ec6b931065dc6565713babe9dc
Rajnandan Kumar <rajnandankumar289@gmail.com>
2021-10-08 22:37:57 +0530

    Fixed Issue #6. Assign #hactoberfestresolved tag

--- ternary.c
+++ ternary.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-
+#include<conio.h>
 int main()
 {
     int a,b,c;
     printf("input three numbers:");
     scanf("%d%d%d",&a,&b,&c);
     //  working
-    a>b ? b>c ? printf("%d<%d<%d",c,b,a) : printf("%d<%d<%d",b,c,a) : b<c ? printf("%d<%d<%d",a,b,c) : a<c ? printf("%d<%d<%d",a,c,b) : printf("%d<%d<%d",c,a,b);
+    a>b ? b>c ? printf("%d<%d<%d",c,b,a) : printf("%d<%d<%d",b,c,a) : b>c ? printf("%d<%d<%d",a,b,c) : a>c ? printf("%d<%d<%d",a,c,b) : printf("%d<%d<%d",c,a,b);
     return 0;
 }
